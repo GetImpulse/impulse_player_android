074e8775ad32f800bf1161a1c831a313cc581301
Alex Kok <a.kok@webuildapps.com>
2025-04-01 15:15:39 +0200

    Fix remote reference

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
 }
 
 dependencies {
-    implementation(project(":ImpulsePlayer")) // Local
-//    implementation("com.github.getimpulse:ImpulsePlayer:0.2.1") // Remote
+//    implementation(project(":ImpulsePlayer")) // Local
+    implementation("com.github.getimpulse:impulse_player_android:0.2.1") // Remote
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
